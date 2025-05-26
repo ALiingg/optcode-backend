640331c62272f762e8b71930fa719b0be8ba49dd
ALing <2933779046@qq.com>
2025-05-26 16:07:36 +0800

    init

---
+++ src/main/java/com/example/optbackend/OptBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.optbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptBackendApplication.class, args);
+    }
+
+}
